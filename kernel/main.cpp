cf64fb58a48e26bd3238bbfc8f383bab7808c5fd
tgw314 <me@k0k1.dev>
2026-02-15 19:06:32 +0900

    Day 04c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <cstdint>
+#include <cstddef>
 
 #include "frame_buffer_config.hpp"
 
@@ -12,42 +13,74 @@ struct PixelColor {
     uint8_t r, g, b;
 };
 
-/** WritePixel は1つの点を描画する
- * @retval     0 成功
- * @retval not 0 失敗
- */
+class PixelWriter {
+   public:
+    PixelWriter(const FrameBufferConfig &config) : config_{config} {}
+    virtual ~PixelWriter() = default;
+    virtual void Write(int x, int y, const PixelColor &c) = 0;
 
-int WritePixel(const FrameBufferConfig &config, int x, int y,
-               const PixelColor &c) {
-    const int pixel_position = x + config.pixels_per_scan_line * y;
-    uint8_t *p = &config.frame_buffer[4 * pixel_position];
-
-    switch (config.pixel_format) {
-        case kPixelRGBResv8BitPerColor:
-            p[0] = c.r;
-            p[1] = c.g;
-            p[2] = c.b;
-            break;
-        case kPixelBGRResv8BitPerColor:
-            p[0] = c.b;
-            p[1] = c.g;
-            p[2] = c.r;
-            break;
-        default:
-            return -1;
+   protected:
+    uint8_t *PixelAt(int x, int y) {
+        return config_.frame_buffer +
+               4 * (x + config_.pixels_per_scan_line * y);
     }
-    return 0;
-}
+
+   private:
+    const FrameBufferConfig &config_;
+};
+
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+   public:
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor &c) override {
+        auto p = PixelAt(x, y);
+        p[0] = c.r;
+        p[1] = c.g;
+        p[2] = c.b;
+    }
+};
+
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+   public:
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor &c) override {
+        auto p = PixelAt(x, y);
+        p[0] = c.b;
+        p[1] = c.g;
+        p[2] = c.r;
+    }
+};
+
+void *operator new(size_t size, void *buf) { return buf; }
+
+// sized-deallocation が LLVM 19 から有効になったので size 引数を追加した
+void operator delete(void *obj, size_t size) noexcept {}
+
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter *pixel_writer;
 
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
+    switch (frame_buffer_config.pixel_format) {
+        case kPixelRGBResv8BitPerColor:
+            pixel_writer = new (pixel_writer_buf)
+                RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+        case kPixelBGRResv8BitPerColor:
+            pixel_writer = new (pixel_writer_buf)
+                BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+    }
+
     for (int x = 0; x < frame_buffer_config.horizontal_resolution; x++) {
         for (int y = 0; y < frame_buffer_config.vertical_resolution; y++) {
-            WritePixel(frame_buffer_config, x, y, {255, 255, 255});
+            pixel_writer->Write(x, y, {255, 255, 255});
         }
     }
     for (int x = 0; x < 200; x++) {
         for (int y = 0; y < 100; y++) {
-            WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+            pixel_writer->Write(100 + x, 100 + y, {0, 255, 0});
         }
     }
     for (;;) __asm__("hlt");
