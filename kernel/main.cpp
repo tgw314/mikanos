2d76d902df745e6799c9ac45800c019caa410c89
tgw314 <me@k0k1.dev>
2026-02-16 20:45:27 +0900

    Day 05d

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <cstdio>
 
 #include "font.hpp"
 #include "frame_buffer_config.hpp"
@@ -38,6 +39,11 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
     for (char c = '!'; c <= '~'; c++, i++) {
         WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
     }
+    WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
+
+    char buf[128];
+    sprintf(buf, "1 + 2 = %d", 1 + 2);
+    WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
 
     for (;;) __asm__("hlt");
 }
---
+++ kernel/newlib_support.c
@@ -0,0 +1,4 @@
+#include <stddef.h>
+#include <sys/types.h>
+
+caddr_t sbrk(int incr) { return NULL; }
