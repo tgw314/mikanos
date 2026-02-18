c3406f8e0f6a728b597c71b0a255cba93ff1de7b
tgw314 <me@k0k1.dev>
2026-02-18 16:43:29 +0900

    Day 06a

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -11,6 +11,40 @@ void *operator new(size_t size, void *buf) { return buf; }
 // sized-deallocation が LLVM 19 から有効になったので size 引数を追加した
 void operator delete(void *obj, size_t size) noexcept {}
 
+const PixelColor kDesktopBGColor{45, 118, 237};
+const PixelColor kDesktopFGColor{255, 255, 255};
+
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+    // clang-format off
+    "@              ",
+    "@@             ",
+    "@.@            ",
+    "@..@           ",
+    "@...@          ",
+    "@....@         ",
+    "@.....@        ",
+    "@......@       ",
+    "@.......@      ",
+    "@........@     ",
+    "@.........@    ",
+    "@..........@   ",
+    "@...........@  ",
+    "@............@ ",
+    "@......@@@@@@@@",
+    "@......@       ",
+    "@....@@.@      ",
+    "@...@ @.@      ",
+    "@..@   @.@     ",
+    "@.@    @.@     ",
+    "@@      @.@    ",
+    "@       @.@    ",
+    "         @.@   ",
+    "         @@@   ",
+    // clang-format on
+};
+
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
 
@@ -42,17 +76,34 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
             break;
     }
 
-    for (int x = 0; x < frame_buffer_config.horizontal_resolution; x++) {
-        for (int y = 0; y < frame_buffer_config.vertical_resolution; y++) {
-            pixel_writer->Write(x, y, {255, 255, 255});
+    const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+    const int kFrameHeight = frame_buffer_config.vertical_resolution;
+
+    FillRectangle(*pixel_writer, {0, 0}, {kFrameWidth, kFrameHeight - 50},
+                  kDesktopBGColor);
+    FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth, 50},
+                  {1, 8, 17});
+    FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth / 5, 50},
+                  {80, 80, 80});
+    DrawRectangle(*pixel_writer, {10, kFrameHeight - 40}, {30, 30},
+                  {160, 160, 160});
+
+    console = new (console_buf)
+        Console{*pixel_writer, kDesktopFGColor, kDesktopBGColor};
+
+    printk("Welcome to MikanOS!\n");
+
+    for (int dy = 0; dy < kMouseCursorHeight; dy++) {
+        for (int dx = 0; dx < kMouseCursorWidth; dx++) {
+            switch (mouse_cursor_shape[dy][dx]) {
+                case '@':
+                    pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+                    break;
+                case '.':
+                    pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+                    break;
+            }
         }
-    }
-
-    console =
-        new (console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
-
-    for (int i = 0; i < 27; i++) {
-        printk("printk: line %d\n", i);
     }
 
     for (;;) __asm__("hlt");
