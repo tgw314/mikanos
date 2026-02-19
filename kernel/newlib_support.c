09bff555ec1a79a49f1dca030c1267a675183051
tgw314 <me@k0k1.dev>
2026-02-19 18:11:20 +0900

    Day 06c

--- kernel/newlib_support.c
+++ kernel/newlib_support.c
@@ -2,7 +2,18 @@
 #include <sys/errno.h>
 #include <sys/types.h>
 
+void _exit(void) {
+    for (;;) __asm__("hlt");
+}
+
 caddr_t sbrk(int incr) {
     errno = ENOMEM;
     return (caddr_t)-1;
+}
+
+int getpid(void) { return 1; }
+
+int kill(int pid, int sig) {
+    errno = EINVAL;
+    return -1;
 }
