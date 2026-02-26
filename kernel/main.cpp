749a53d2a87bf0c38479c43cadc08be5a9cf02f8
tgw314 <me@k0k1.dev>
2026-02-26 20:10:44 +0900

    Day 09a

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -2,6 +2,8 @@
 #include <cstdarg>
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
+#include <memory>
 
 #include "asmfunc.h"
 #include "console.hpp"
@@ -9,6 +11,7 @@
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "interrupt.hpp"
+#include "layer.hpp"
 #include "logger.hpp"
 #include "memory_manager.hpp"
 #include "memory_map.hpp"
@@ -19,10 +22,8 @@
 #include "segment.hpp"
 #include "usb/classdriver/mouse.hpp"
 #include "usb/xhci/xhci.hpp"
+#include "window.hpp"
 #include "x86_descriptor.hpp"
-
-const PixelColor kDesktopBGColor{45, 118, 237};
-const PixelColor kDesktopFGColor{255, 255, 255};
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
@@ -46,11 +47,12 @@ int printk(const char *format, ...) {
 char memory_manager_buf[sizeof(BitmapMemoryManager)];
 BitmapMemoryManager *memory_manager;
 
-char mouse_cursor_buf[sizeof(MouseCursor)];
-MouseCursor *mouse_cursor;
+unsigned int mouse_layer_id;
 
 void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
-    mouse_cursor->MoveRelative({displacement_x, displacement_y});
+    layer_manager->MoveRelative(mouse_layer_id,
+                                {displacement_x, displacement_y});
+    layer_manager->Draw();
 }
 
 void SwitchEhci2Xhci(const pci::Device &xhc_dev) {
@@ -107,20 +109,10 @@ extern "C" void KernelMainNewStack(
             break;
     }
 
-    const int kFrameWidth = frame_buffer_config.horizontal_resolution;
-    const int kFrameHeight = frame_buffer_config.vertical_resolution;
+    DrawDesktop(*pixel_writer);
 
-    FillRectangle(*pixel_writer, {0, 0}, {kFrameWidth, kFrameHeight - 50},
-                  kDesktopBGColor);
-    FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth, 50},
-                  {1, 8, 17});
-    FillRectangle(*pixel_writer, {0, kFrameHeight - 50}, {kFrameWidth / 5, 50},
-                  {80, 80, 80});
-    DrawRectangle(*pixel_writer, {10, kFrameHeight - 40}, {30, 30},
-                  {160, 160, 160});
-
-    console = new (console_buf)
-        Console{*pixel_writer, kDesktopFGColor, kDesktopBGColor};
+    console = new (console_buf) Console{kDesktopFGColor, kDesktopBGColor};
+    console->SetWriter(pixel_writer);
 
     printk("Welcome to MikanOS!\n");
     SetLogLevel(kInfo);
@@ -162,8 +154,11 @@ extern "C" void KernelMainNewStack(
     memory_manager->SetMemoryRange(FrameID{1},
                                    FrameID{available_end / kBytesPerFrame});
 
-    mouse_cursor = new (mouse_cursor_buf)
-        MouseCursor{pixel_writer, kDesktopBGColor, {300, 200}};
+    if (auto err = InitializeHeap(*memory_manager)) {
+        Log(kError, "failed to allocate pages: %s at %s:%d\n", err.Name(),
+            err.File(), err.Line());
+        exit(1);
+    }
 
     std::array<Message, 32> main_queue_data;
     ArrayQueue<Message> main_queue{main_queue_data};
@@ -247,6 +242,32 @@ extern "C" void KernelMainNewStack(
             }
         }
     }
+
+    const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+    const int kFrameHeight = frame_buffer_config.vertical_resolution;
+
+    auto bgwindow = std::make_shared<Window>(kFrameWidth, kFrameHeight);
+    auto bgwriter = bgwindow->Writer();
+
+    DrawDesktop(*bgwriter);
+    console->SetWriter(bgwriter);
+
+    auto mouse_window =
+        std::make_shared<Window>(kMouseCursorWidth, kMouseCursorHeight);
+    mouse_window->SetTransparentColor(kMouseTransparentColor);
+    DrawMouseCursor(mouse_window->Writer(), {0, 0});
+
+    layer_manager = new LayerManager;
+    layer_manager->SetWriter(pixel_writer);
+
+    auto bglayer_id =
+        layer_manager->NewLayer().SetWindow(bgwindow).Move({0, 0}).ID();
+    mouse_layer_id =
+        layer_manager->NewLayer().SetWindow(mouse_window).Move({200, 200}).ID();
+
+    layer_manager->UpDown(bglayer_id, 0);
+    layer_manager->UpDown(mouse_layer_id, 1);
+    layer_manager->Draw();
 
     for (;;) {
         __asm__("cli");
---
+++ kernel/window.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <optional>
+#include <vector>
+
+#include "graphics.hpp"
+
+/** Window クラスはグラフィックの表示領域を表す
+ *
+ * タイトルやメニューがあるウィンドウだけでなく,
+ * マウスカーソルの表示領域なども対象とする
+ */
+class Window {
+   public:
+    // WindowWriter は Window と関連付けられた PixelWriter を提供する
+    class WindowWriter : public PixelWriter {
+       public:
+        WindowWriter(Window &window) : window_{window} {}
+        // 指定された位置に指定された色を描く
+        virtual void Write(int x, int y, const PixelColor &c) override {
+            window_.At(x, y) = c;
+        }
+        // Width は関連付けられた Window の横幅をピクセル単位で返す
+        virtual int Width() const override { return window_.Width(); }
+        // Height は関連付けられた Window の高さをピクセル単位で返す
+        virtual int Height() const override { return window_.Height(); }
+
+       private:
+        Window &window_;
+    };
+
+    // 指定されたピクセル数の平面描画領域を作成する
+    Window(int width, int height);
+    ~Window() = default;
+    Window(const Window &rhs) = delete;
+    Window &operator=(const Window &rhs) = delete;
+
+    /** 与えられた PixelWriter にこのウィンドウの表示領域を描画する。
+     *
+     * writer  描画先
+     * position  writer の左上を基準とした描画位置
+     */
+    void DrawTo(PixelWriter &writer, Vector2D<int> position);
+    // 透過色を設定する
+    void SetTransparentColor(std::optional<PixelColor> c);
+    // このインスタンスに紐付いた WindowWriter を取得する
+    WindowWriter *Writer();
+
+    // 指定した位置のピクセルを返す
+    PixelColor &At(int x, int y);
+    // 指定した位置のピクセルを返す
+    const PixelColor &At(int x, int y) const;
+
+    // 平面描画領域の横幅をピクセル単位で返す
+    int Width() const;
+    // 平面描画領域の高さをピクセル単位で返す
+    int Height() const;
+
+   private:
+    int width_, height_;
+    std::vector<std::vector<PixelColor>> data_{};
+    WindowWriter writer_{*this};
+    std::optional<PixelColor> transparent_color_{std::nullopt};
+};
