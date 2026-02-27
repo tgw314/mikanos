00682fe54aa4c5f8dab64fdf26c96c144a42e51b
tgw314 <me@k0k1.dev>
2026-02-27 16:57:13 +0900

    Day 09b

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -20,6 +20,7 @@
 #include "pci.hpp"
 #include "queue.hpp"
 #include "segment.hpp"
+#include "timer.hpp"
 #include "usb/classdriver/mouse.hpp"
 #include "usb/xhci/xhci.hpp"
 #include "window.hpp"
@@ -52,7 +53,11 @@ unsigned int mouse_layer_id;
 void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
     layer_manager->MoveRelative(mouse_layer_id,
                                 {displacement_x, displacement_y});
+    StartLAPICTimer();
     layer_manager->Draw();
+    auto elapsed = LAPICTimerElapsed();
+    StopLAPICTimer();
+    printk("MouseObserver: elapsed = %u\n", elapsed);
 }
 
 void SwitchEhci2Xhci(const pci::Device &xhc_dev) {
@@ -116,6 +121,8 @@ extern "C" void KernelMainNewStack(
 
     printk("Welcome to MikanOS!\n");
     SetLogLevel(kInfo);
+
+    InitializeLAPICTimer();
 
     SetupSegments();
 
