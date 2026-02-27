16a2833dc357bfa8d5242fcdb5686aeb43f369cf
tgw314 <me@k0k1.dev>
2026-02-27 18:57:25 +0900

    Revert "feat(console): increase console size to 67x120"
    This reverts commit c57688d3612e244254d06624088a783dee27c958.

--- kernel/console.hpp
+++ kernel/console.hpp
@@ -4,7 +4,7 @@
 
 class Console {
    public:
-    static const int kRows = 67, kColumns = 120;
+    static const int kRows = 25, kColumns = 80;
 
     Console(const PixelColor &fg_color, const PixelColor &bg_color);
     void PutString(const char *s);
