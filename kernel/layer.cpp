b32f8c10b6fd3cfb1cc821cbe7ed218cfaed36b7
tgw314 <me@k0k1.dev>
2026-03-02 15:23:31 +0900

    Day 10e

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 
 #include "frame_buffer.hpp"
+#include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "window.hpp"
 
@@ -36,7 +37,13 @@ void Layer::DrawTo(FrameBuffer &screen, const Rectangle<int> &area) const {
     }
 }
 
-void LayerManager::SetWriter(FrameBuffer *screen) { screen_ = screen; }
+void LayerManager::SetWriter(FrameBuffer *screen) {
+    screen_ = screen;
+
+    FrameBufferConfig back_config = screen->Config();
+    back_config.frame_buffer = nullptr;
+    back_buffer_.Initialize(back_config);
+}
 
 Layer &LayerManager::NewLayer() {
     latest_id_++;
@@ -45,8 +52,9 @@ Layer &LayerManager::NewLayer() {
 
 void LayerManager::Draw(const Rectangle<int> &area) const {
     for (auto layer : layer_stack_) {
-        layer->DrawTo(*screen_, area);
+        layer->DrawTo(back_buffer_, area);
     }
+    screen_->Copy(area.pos, back_buffer_, area);
 }
 
 void LayerManager::Draw(unsigned int id) const {
@@ -59,9 +67,10 @@ void LayerManager::Draw(unsigned int id) const {
             draw = true;
         }
         if (draw) {
-            layer->DrawTo(*screen_, window_area);
+            layer->DrawTo(back_buffer_, window_area);
         }
     }
+    screen_->Copy(window_area.pos, back_buffer_, window_area);
 }
 
 void LayerManager::Move(unsigned int id, Vector2D<int> new_pos) {
