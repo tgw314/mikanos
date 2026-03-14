02e60858724dee6dafa6784bad2f466046fb08ee
tgw314 <me@k0k1.dev>
2026-03-14 18:53:36 +0900

    Day 14d

--- kernel/task.cpp
+++ kernel/task.cpp
@@ -18,6 +18,10 @@ void Erase(T &c, const U &value) {
     auto it = std::remove(c.begin(), c.end(), value);
     c.erase(it, c.end());
 }
+
+void TaskIdle(uint64_t task_id, int64_t data) {
+    for (;;) __asm__("hlt");
+}
 }  // namespace
 
 Task::Task(uint64_t id) : id_{id}, msgs_{} {}
@@ -74,6 +78,10 @@ std::optional<Message> Task::ReceiveMessage() {
 TaskManager::TaskManager() {
     Task &task = NewTask().SetLevel(current_level_).SetRunning(true);
     running_[current_level_].push_back(&task);
+
+    Task &idle =
+        NewTask().InitContext(TaskIdle, 0).SetLevel(0).SetRunning(true);
+    running_[0].push_back(&idle);
 }
 
 Task &TaskManager::NewTask() {
