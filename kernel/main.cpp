98a45e5a465682e283390eeb4ae2a734b70695ee
tgw314 <me@k0k1.dev>
2026-03-18 18:44:06 +0900

    Day 15c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -21,6 +21,7 @@
 #include "pci.hpp"
 #include "segment.hpp"
 #include "task.hpp"
+#include "terminal.hpp"
 #include "timer.hpp"
 #include "usb/xhci/xhci.hpp"
 #include "window.hpp"
@@ -67,7 +68,7 @@ void InitializeTextWindow() {
     text_window_layer_id = layer_manager->NewLayer()
                                .SetWindow(text_window)
                                .SetDraggable(true)
-                               .Move({350, 200})
+                               .Move({500, 100})
                                .ID();
 
     layer_manager->UpDown(text_window_layer_id,
@@ -193,6 +194,8 @@ extern "C" void KernelMainNewStack(
     Task &main_task = task_manager->CurrentTask();
     const uint64_t taskb_id =
         task_manager->NewTask().InitContext(TaskB, 45).Wakeup().ID();
+    const uint64_t task_terminal_id =
+        task_manager->NewTask().InitContext(TaskTerminal, 0).Wakeup().ID();
 
     usb::xhci::Initialize();
     InitializeKeyboard();
@@ -236,6 +239,10 @@ extern "C" void KernelMainNewStack(
                     textbox_cursor_visible = !textbox_cursor_visible;
                     DrawTextCursor(textbox_cursor_visible);
                     layer_manager->Draw(text_window_layer_id);
+
+                    __asm__("cli");
+                    task_manager->SendMessage(task_terminal_id, *msg);
+                    __asm__("sti");
                 }
                 break;
             case Message::kKeyPush: {
