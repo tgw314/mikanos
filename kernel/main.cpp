fcf18fa1f6076980bcb2a96cabcbe5a6a9b5b80f
tgw314 <me@k0k1.dev>
2026-03-22 17:26:48 +0900

    Day 17a

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -109,7 +109,8 @@ alignas(16) uint8_t kernel_main_stack[1024 * 1024];
 
 extern "C" void KernelMainNewStack(
     const FrameBufferConfig &frame_buffer_config_ref,
-    const MemoryMap &memory_map_ref, const acpi::RSDP &acpi_table) {
+    const MemoryMap &memory_map_ref, const acpi::RSDP &acpi_table,
+    void *volume_image) {
     MemoryMap memory_map{memory_map_ref};
 
     InitializeGraphics(frame_buffer_config_ref);
@@ -146,6 +147,18 @@ extern "C" void KernelMainNewStack(
     usb::xhci::Initialize();
     InitializeKeyboard();
     InitializeMouse();
+
+    uint8_t *p = reinterpret_cast<uint8_t *>(volume_image);
+    printk("Volume Image:\n");
+    for (int i = 0; i < 16; i++) {
+        printk("%04x:", i * 16);
+
+        for (int j = 0; j < 8; j++) printk(" %02x", *p++);
+        printk(" ");
+        for (int j = 0; j < 8; j++) printk(" %02x", *p++);
+
+        printk("\n");
+    }
 
     for (;;) {
         char str[128];
