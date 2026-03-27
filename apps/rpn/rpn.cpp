e04cec1cb577e601e802daa698266c116391c096
tgw314 <me@k0k1.dev>
2026-03-27 19:23:11 +0900

    Day 20a

--- apps/rpn/rpn.cpp
+++ apps/rpn/rpn.cpp
@@ -1,5 +1,15 @@
 #include <cstdlib>
 #include <cstring>
+
+#include "../../kernel/graphics.hpp"
+
+auto &printk =
+    *reinterpret_cast<int (*)(const char *, ...)>(0x000000000010cc90);
+auto &fill_rect =
+    *reinterpret_cast<decltype(FillRectangle) *>(0x000000000010dd00);
+auto &scrn_writer =
+    *reinterpret_cast<decltype(screen_writer) *>(0x000000000024d068);
+
 int stack_ptr;
 long stack[100];
 
@@ -14,18 +24,25 @@ extern "C" int main(int argc, char **argv) {
             long b = Pop();
             long a = Pop();
             Push(a + b);
+            printk("[%d] <- %ld\n", stack_ptr, a + b);
             continue;
         }
         if (strcmp(argv[i], "-") == 0) {
             long b = Pop();
             long a = Pop();
             Push(a - b);
+            printk("[%d] <- %ld\n", stack_ptr, a - b);
             continue;
         }
 
         long a = atol(argv[i]);
         Push(a);
+        printk("[%d] <- %ld\n", stack_ptr, a);
     }
+
+    fill_rect(*scrn_writer, Vector2D<int>{100, 10}, Vector2D<int>{200, 200},
+              ToColor(0x00ff00));
+
     if (stack_ptr < 0) {
         return 0;
     }
