9238008180e273a0f8e3e3e5929af0a17ba4cee1
tgw314 <me@k0k1.dev>
2026-04-02 20:52:35 +0900

    Day 22a

--- apps/large/large.cpp
+++ apps/large/large.cpp
@@ -4,4 +4,4 @@
 
 char table[3 * 1024 * 1024];
 
-extern "C" void main(int argc, char **argv) { SyscallExit(atoi(argv[1])); }
+extern "C" void main(int argc, char **argv) { exit(atoi(argv[1])); }
--- apps/newlib_support.c
+++ apps/newlib_support.c
@@ -1,9 +1,10 @@
 #include <errno.h>
 #include <stddef.h>
-#include <stdint.h>
 #include <sys/errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+
+#include "syscall.h"
 
 int close(int fd) {
     errno = EBADF;
@@ -35,17 +36,13 @@ caddr_t sbrk(int incr) {
     return (caddr_t)-1;
 }
 
-struct SyscallResult {
-    uint64_t value;
-    int error;
-};
-struct SyscallResult SyscallPutString(uint64_t, uint64_t, uint64_t);
-
 ssize_t write(int fd, const void *buf, size_t count) {
-    struct SyscallResult res = SyscallPutString(fd, (uint64_t)buf, count);
+    struct SyscallResult res = SyscallPutString(fd, buf, count);
     if (res.error == 0) {
         return res.value;
     }
     errno = res.error;
     return -1;
 }
+
+void _exit(int status) { SyscallExit(status); }
