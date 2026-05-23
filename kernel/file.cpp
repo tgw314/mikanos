addaf1b75dbd4d33816894655e8cd921edd767e2
tgw314 <me@k0k1.dev>
2026-05-23 19:42:09 +0900

    Day 29d

--- kernel/file.cpp
+++ kernel/file.cpp
@@ -1,6 +1,7 @@
 #include "file.hpp"
 
 #include <cstdarg>
+#include <cstddef>
 #include <cstdio>
 
 size_t PrintToFD(FileDescriptor &fd, const char *format, ...) {
@@ -14,4 +15,19 @@ size_t PrintToFD(FileDescriptor &fd, const char *format, ...) {
 
     fd.Write(s, result);
     return result;
+}
+
+size_t ReadDelim(FileDescriptor &fd, char delim, char *buf, size_t len) {
+    for (size_t i = 0; i < len - 1; i++) {
+        if (fd.Read(&buf[i], 1) == 0) {
+            buf[i] = '\0';
+            return i;
+        }
+        if (buf[i] == delim) {
+            buf[++i] = '\0';
+            return i;
+        }
+    }
+    buf[len - 1] = '\0';
+    return len - 1;
 }
--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -506,19 +506,13 @@ void Terminal::ExecuteLine() {
         }
 
         fat::FileDescriptor fd{*file_entry};
-        char u8buf[5];
+        char u8buf[1024];
 
         DrawCursor(false);
         for (;;) {
-            if (fd.Read(&u8buf[0], 1) != 1) {
+            if (ReadDelim(fd, '\n', u8buf, sizeof(u8buf)) == 0) {
                 break;
             }
-            const int u8_remain = CountUTF8Size(u8buf[0]) - 1;
-            if (u8_remain > 0 && fd.Read(&u8buf[1], u8_remain) != u8_remain) {
-                break;
-            }
-            u8buf[u8_remain + 1] = '\0';
-
             PrintToFD(*files_[1], "%s", u8buf);
         }
         DrawCursor(true);
@@ -688,6 +682,17 @@ void Terminal::Print(const char *s, std::optional<size_t> len) {
     __asm__("sti");
 }
 
+void Terminal::Redraw() {
+    Rectangle<int> draw_area{ToplevelWindow::kTopLeftMargin,
+                             window_->InnerSize()};
+
+    Message msg = MakeLayerMessage(task_.ID(), LayerID(),
+                                   LayerOperation::DrawArea, draw_area);
+    __asm__("cli");
+    task_manager->SendMessage(1, msg);
+    __asm__("sti");
+}
+
 Rectangle<int> Terminal::HistoryUpDown(int direction) {
     if (direction == -1 && cmd_history_index_ >= 0) {
         cmd_history_index_--;
@@ -828,12 +833,14 @@ size_t TerminalFileDescriptor::Read(void *buf, size_t len) {
 
         bufc[0] = msg->arg.keyboard.ascii;
         term_.Print(bufc, 1);
+        term_.Redraw();
         return 1;
     }
 }
 
 size_t TerminalFileDescriptor::Write(const void *buf, size_t len) {
     term_.Print(reinterpret_cast<const char *>(buf), len);
+    term_.Redraw();
     return len;
 }
 
